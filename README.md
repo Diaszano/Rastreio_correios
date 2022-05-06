d8342700863c19692d396f68114538a723c7d948
Diaszano <lucasdiiassantos@gmail.com>
2022-05-06 03:45:23 -0300

    projeto iniciado

---
+++ README.md
@@ -0,0 +1,16 @@
+# 🐍 Rastreador de Encomendas
+
+## Esse programa faz o rastreamento de encomendas dos correios
+
+- Rastreio de encomendas
+- Armazenamento no banco de dados
+
+### O que usamos na infraestrutura?
+- [Utilizamos banco de dados SQLite para o armazenamento dos dados](https://www.sqlite.org/index.html)
+- [Utilizamos a linguagem Python](https://www.python.org/)
+
+### O que já foi implementado?
+
+- [x] Rastreio de encomendas
+- [ ] Armazenamento no banco de dados
+- [ ] Interfaces agradáveis
---
+++ database.py
@@ -0,0 +1,168 @@
+#-----------------------
+# BIBLIOTECAS
+#-----------------------
+import os
+import sqlite3
+#-----------------------
+# CLASSES
+#-----------------------
+class DataBase():
+    def __init__(self,nome:str="./data/database.db") -> None:
+        arquivo = os.path.basename(nome);
+        pasta   = nome.replace(arquivo,'');
+        if arquivo == '':
+            arquivo = 'database.db';
+        if pasta == '':
+            pasta = './data/';
+        if(not(os.path.exists(pasta))):
+            os.mkdir(pasta);
+        if(not('/data/' in pasta)):
+            self.nome = f'{pasta}/data/';
+            os.mkdir(self.nome);
+            self.nome = f'{self.nome}{arquivo}';
+        else:
+            self.nome = nome;
+
+    def conexao(self) -> None:
+        self.connection = sqlite3.connect(self.nome);
+    
+    def desconexao(self) -> None:
+        try:
+            self.connection.close();
+        except:
+            pass;
+    
+    def creat_table(self,comando:str='') -> None:
+        if(comando == ''):
+            comando =   """ CREATE TABLE IF NOT EXISTS encomenda(
+                            id              INTEGER primary key autoincrement,
+                            id_user         INTEGER NOT NULL,
+                            codigo          TEXT NOT NULL,
+                            nome_rastreio   TEXT,
+                            data            TEXT NOT NULL,
+                            informacoes     TEXT NOT NULL)
+                        """;
+        try:
+            self.conexao();
+            Connection = self.connection;
+            cursor = Connection.cursor();
+            cursor.execute(comando);
+            Connection.commit();
+            print("Comando efetuado com sucesso", cursor.rowcount);
+            cursor.close();
+        except sqlite3.Error as error:
+            print("Falha do comando", error);
+        finally:
+            if Connection:
+                Connection.close();
+                print("Conexão com SQLite está fechada");
+
+    def verifica(self,comando:str='',id_user:str='',codigo:str='')->bool:
+        print(id_user,codigo);
+        if(id_user == '' or codigo == ''):
+            return False;
+        if(comando == ''):
+            comando =   f""" 
+                            SELECT *
+                            FROM encomenda
+                            WHERE id_user='{id_user}' AND codigo='{codigo}'
+                        """;
+        try:
+            self.conexao();
+            Connection = self.connection;
+            cursor = Connection.cursor();
+            print("Conexão com SQLite efetuada com sucesso");
+            cursor.execute(comando);
+            print("Comando efetuado com sucesso", cursor.rowcount);
+            if cursor.fetchall() != []:
+                cursor.close();
+                return True;
+            cursor.close();
+            return False;
+        except sqlite3.Error as error:
+            print("Falha do comando", error);
+        finally:
+            if Connection:
+                Connection.close();
+                print("Conexão com SQLite está fechada");
+    
+    def insert(self,comando:str='',comando_tuple=[]) -> None:
+        if(comando == ''):
+            comando =   """ 
+                            INSERT INTO encomenda
+                            (id_user, codigo, nome_rastreio, 'data', informacoes)
+                            VALUES(?, ?, ?,(SELECT DATETIME('now','localtime')), ?)
+                        """;
+        if(comando_tuple == []):
+            comando_tuple = ('id_user','codigo','nome_rastreio','data','informacoes');
+        if(self.verifica(id_user=comando_tuple[0],codigo=comando_tuple[1])):
+            return;
+        try:
+            self.conexao();
+            Connection = self.connection;
+            cursor = Connection.cursor();
+            print("Conexão com SQLite efetuada com sucesso");
+            cursor.execute(comando, comando_tuple);
+            Connection.commit();
+            print("Comando efetuado com sucesso", cursor.rowcount);
+            cursor.close();
+        except sqlite3.Error as error:
+            print("Falha do comando", error);
+        finally:
+            if Connection:
+                Connection.close();
+                print("Conexão com SQLite está fechada");
+
+    def upadate(self,id_user:str='',codigo:str='',comando:str='',mensagem:str='') -> bool:
+        if(comando == ''):
+            comando =   f""" UPDATE encomenda
+                            SET 'data'=(SELECT DATETIME('now','localtime')), 
+                            informacoes='{mensagem}'
+                            WHERE id_user='{id_user}' AND codigo='{codigo}'
+                        """;
+        try:
+            self.conexao();
+            Connection = self.connection;
+            cursor = Connection.cursor();
+            print("Conexão com SQLite efetuada com sucesso");
+            cursor.execute(comando);
+            Connection.commit();
+            print("Comando efetuado com sucesso", cursor.rowcount);
+            cursor.close();
+        except sqlite3.Error as error:
+            print("Falha do comando", error);
+        finally:
+            if Connection:
+                Connection.close();
+                print("Conexão com SQLite está fechada");
+    
+    def delete(self,id_user:str='',codigo:str='') -> bool:
+        if(comando == ''):
+            comando =   """ INSERT INTO SqliteDb_developers
+                            (id, name, email, joining_date, salary) 
+                            VALUES (?, ?, ?, ?, ?);
+                        """;
+        if(comando_tuple == []):
+            comando_tuple = ('id', 'name', 'email', 'joinDate', 'salary');
+        try:
+            self.conexao();
+            Connection = self.connection;
+            cursor = Connection.cursor();
+            print("Conexão com SQLite efetuada com sucesso");
+            cursor.execute(comando, comando_tuple);
+            Connection.commit();
+            print("Comando efetuado com sucesso", cursor.rowcount);
+            cursor.close();
+        except sqlite3.Error as error:
+            print("Falha do comando", error);
+        finally:
+            if Connection:
+                Connection.close();
+                print("Conexão com SQLite está fechada");
+#-----------------------
+# MAIN()
+#-----------------------
+if(__name__ == "__main__"):
+    db = DataBase();
+    db.creat_table();
+#-----------------------
